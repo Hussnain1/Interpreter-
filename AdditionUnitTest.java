509e31e483e8eb4fcce18676a49160f16b8859f8
Hussnain Cheema <hussnain_93@hotmail.com>
2015-02-25 22:37:40 +0500

    Update AdditionUnitTest.java

--- AdditionUnitTest.java
+++ AdditionUnitTest.java
@@ -5,7 +5,7 @@ public class AdditionUnitTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		interpreter A = new interpreter();
-		A.readFromFile("D:\\SampleFile3.txt");
+		A.readFromFile("D:\\SampleFile2.txt");
 		
 	}
 	
