ad043ba20f017355ca20832b600a4019d09ed284
Hussnain Cheema <hussnain_93@hotmail.com>
2015-02-25 22:33:07 +0500

    Create AdditionUnitTest.java

---
+++ AdditionUnitTest.java
@@ -0,0 +1,12 @@
+package lab2;
+
+public class AdditionUnitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		interpreter A = new interpreter();
+		A.readFromFile("D:\\SampleFile3.txt");
+		
+	}
+	
+}
