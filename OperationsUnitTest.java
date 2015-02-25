2e2750e0e068d650f76796d85e7b42db7e586ef3
Hussnain Cheema <hussnain_93@hotmail.com>
2015-02-25 22:36:50 +0500

    Update OperationsUnitTest.java

--- OperationsUnitTest.java
+++ OperationsUnitTest.java
@@ -5,7 +5,7 @@ public class OperationsUnitTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		interpreter A = new interpreter();
-		A.readFromFile("D:\\SampleFile4.txt");
+		A.readFromFile("D:\\SampleFile3.txt");
 		
 		
 		
